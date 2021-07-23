c83985e9970226f8749c75062e05297f358b1c34
Oleg Chuy <misterchuy21@gmail.com>
2021-07-23 14:07:31 +0300

    Has been added lombok notations.

--- src/main/java/model/entities/Answer.java
+++ src/main/java/model/entities/Answer.java
@@ -1,5 +1,15 @@
 package model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Answer {
 
     private int answer_id;
@@ -9,92 +19,4 @@ public class Answer {
     private String answer_3;
     private int correct_answer_num;
 
-    public Answer() {
-    }
-
-    public Answer(String answer_1, String answer_2, String answer_3, int correct_answer_num) {
-        this.answer_1 = answer_1;
-        this.answer_2 = answer_2;
-        this.answer_3 = answer_3;
-        this.correct_answer_num = correct_answer_num;
-    }
-
-    public Answer(int question_id, String answer_1, String answer_2, String answer_3, int correct_answer_num) {
-        this.question_id = question_id;
-        this.answer_1 = answer_1;
-        this.answer_2 = answer_2;
-        this.answer_3 = answer_3;
-        this.correct_answer_num = correct_answer_num;
-    }
-
-    public Answer(int answer_id, int question_id, String answer_1, String answer_2, String answer_3, int correct_answer_num) {
-        this.answer_id = answer_id;
-        this.question_id = question_id;
-        this.answer_1 = answer_1;
-        this.answer_2 = answer_2;
-        this.answer_3 = answer_3;
-        this.correct_answer_num = correct_answer_num;
-    }
-
-    public int getAnswer_id() {
-        return answer_id;
-    }
-
-    public void setAnswer_id(int answer_id) {
-        this.answer_id = answer_id;
-    }
-
-    public int getQuestion_id() {
-        return question_id;
-    }
-
-    public void setQuestion_id(int question_id) {
-        this.question_id = question_id;
-    }
-
-    public String getAnswer_1() {
-        return answer_1;
-    }
-
-    public void setAnswer_1(String answer_1) {
-        this.answer_1 = answer_1;
-    }
-
-    public String getAnswer_2() {
-        return answer_2;
-    }
-
-    public void setAnswer_2(String answer_2) {
-        this.answer_2 = answer_2;
-    }
-
-    public String getAnswer_3() {
-        return answer_3;
-    }
-
-    public void setAnswer_3(String answer_3) {
-        this.answer_3 = answer_3;
-    }
-
-    public int getCorrect_answer_num() {
-        return correct_answer_num;
-    }
-
-    public void setCorrect_answer_num(int correct_answer_num) {
-        if (correct_answer_num >= 1 && correct_answer_num <= 3) {
-            this.correct_answer_num = correct_answer_num;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Answer{" +
-                "answer_id=" + answer_id +
-                ", question_id=" + question_id +
-                ", answer_1='" + answer_1 + '\'' +
-                ", answer_2='" + answer_2 + '\'' +
-                ", answer_3='" + answer_3 + '\'' +
-                ", correct_answer_num=" + correct_answer_num +
-                '}';
-    }
 }
--- src/main/java/model/entities/Question.java
+++ src/main/java/model/entities/Question.java
@@ -1,58 +1,18 @@
 package model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Question {
 
     private int question_id;
     private int category_id;
     private String question_text;
 
-    public Question() {}
-
-    public Question(String question_text) {
-        this.question_text = question_text;
-    }
-
-    public Question(int category_id, String question_text) {
-        this.category_id = category_id;
-        this.question_text = question_text;
-    }
-
-    public Question(int question_id, int category_id, String question_text) {
-        this.question_id = question_id;
-        this.category_id = category_id;
-        this.question_text = question_text;
-    }
-
-    public int getQuestion_id() {
-        return question_id;
-    }
-
-    public void setQuestion_id(int question_id) {
-        this.question_id = question_id;
-    }
-
-    public int getCategory_id() {
-        return category_id;
-    }
-
-    public void setCategory_id(int category_id) {
-        this.category_id = category_id;
-    }
-
-    public String getQuestion_text() {
-        return question_text;
-    }
-
-    public void setQuestion_text(String question_text) {
-        this.question_text = question_text;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "question_id=" + question_id +
-                ", category_id=" + category_id +
-                ", question_text='" + question_text + '\'' +
-                '}';
-    }
 }
